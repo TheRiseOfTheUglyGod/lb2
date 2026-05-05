1105c7cc4fa0b9292b89d26f2e04624c6abd0a05
TheRiseOfTheUglyGod <pajiloy.danil@mail.ru>
2026-05-05 16:50:19 +0300

    vsem svo

--- src/main/java/ru/kafpin/lb2/TriangleController.java
+++ src/main/java/ru/kafpin/lb2/TriangleController.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-
+//4mo
 import java.net.URL;
 import java.util.ResourceBundle;
 
